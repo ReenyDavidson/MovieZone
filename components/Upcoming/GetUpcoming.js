62f3c2065e13087cd9c23e70fdbbb1f9566bc031
Uzodike Oguejiofor <reenydavidson@gmail.com>
2022-01-12 23:10:29 +0100

    creating upcoming screens

---
+++ components/Upcoming/GetUpcoming.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const GetUpcoming = () => {
+  return (
+    <View>
+      <Text>Upcoming</Text>
+    </View>
+  );
+};
+
+export default GetUpcoming;
